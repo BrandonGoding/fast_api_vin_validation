787812ea8d5d302d9d289727e297af89f1d2be0b
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-12-09 16:19:16 -0500

    updated Redundent naming

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ from decouple import config
 from starlette.middleware.cors import CORSMiddleware as CORSMiddleware
 
 
-class VinNumber(BaseModel):
-    vin_number: str
+class VehicleIdentificationNumber(BaseModel):
+    vehicle_identification_number: str
 
 
 class VinResponse(BaseModel):
@@ -22,10 +22,10 @@ database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
 vin_numbers = sqlalchemy.Table(
-    "vinNumbers",
+    "vehicleIdentificationNumbers",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("vin_number", sqlalchemy.String(100), index=True, unique=True),
+    sqlalchemy.Column("vehicle_identification_number", sqlalchemy.String(100), index=True, unique=True),
 )
 
 
@@ -60,10 +60,10 @@ async def hello_world():
 
 
 @app.post("/", response_model=VinResponse)
-async def validate_vin(vin_number: VinNumber):
-    query = f"SELECT * FROM vinNumbers WHERE vin_number = :vin_number"
+async def validate_vin(vin: VehicleIdentificationNumber):
+    query = f"SELECT * FROM vehicleIdentificationNumbers WHERE vehicle_identification_number = :vin"
     result = await database.fetch_one(
-        query=query, values={"vin_number": vin_number.vin_number}
+        query=query, values={"vin": vin.vehicle_identification_number}
     )
     if result:
         return {"exists": True}
